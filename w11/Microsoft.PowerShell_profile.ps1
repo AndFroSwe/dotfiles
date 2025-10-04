b1c7c47d42fd256bf746400023d5a5f9ae5233b5
andfro <andfro@protonmail.com>
2025-10-04 09:40:23 +0200

    omp add scratchpad. ps1 remove gitconfig. sp change frame

--- w11/Microsoft.PowerShell_profile.ps1
+++ w11/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,5 @@
 # Stop annoying prediction
 Set-PSReadlineOption -PredictionSource None
 
-# Alias for config files
-function GitConfig {git --git-dir=$HOME/.myconf --work-tree=$HOME}
-Set-Alias -name config -value GitConfig
-
 # Oh My Posh
 oh-my-posh --init --shell pwsh --config ~/.config/oh-my-posh/andfro.omp.json | Invoke-Expression
-
