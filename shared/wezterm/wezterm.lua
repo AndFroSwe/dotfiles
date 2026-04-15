2441484497d8cd43b00452adbdf48b23ccfb4828
andfro <andfro@protonmail.com>
2026-04-15 08:44:16 +0200

    add plugin for pane nav

--- shared/wezterm/wezterm.lua
+++ shared/wezterm/wezterm.lua
@@ -40,22 +40,39 @@ config.window_decorations = "RESIZE"
 config.enable_tab_bar = true
 
 -- Vim aware pane navigation
-local function is_nvim(pane)
+local function is_vim(pane)
 	local proc = pane:get_foreground_process_name()
 	return proc ~= nil and (proc:find("nvim") ~= nil or proc:find("vim") ~= nil)
 end
 
-local function smart_split_nav(key, direction)
+local direction_keys = {
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	-- reverse lookup
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function smart_split_nav(resize_or_move, key)
 	return {
 		key = key,
-		mods = "CTRL",
+		mods = resize_or_move == "resize" and "META" or "CTRL",
 		action = wezterm.action_callback(function(win, pane)
-			if is_nvim(pane) then
-				-- send key to nvim
-				win:perform_action(wezterm.action.SendKey({ key = key, mods = "CTRL" }), pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
 			else
-				-- move wezterm pane
-				win:perform_action(wezterm.action.ActivatePaneDirection(direction), pane)
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
 			end
 		end),
 	}
