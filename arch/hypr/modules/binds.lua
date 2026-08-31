1d8cd0837df2e26d2271ed24e96a34c443d21119
andfro <andfro@protonmail.com>
2026-08-31 20:51:01 +0200

    Comment and fix typo, waybar and hypr

--- arch/hypr/modules/binds.lua
+++ arch/hypr/modules/binds.lua
@@ -82,6 +82,8 @@ hl.bind(
 	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
 	{ locked = true, repeating = true }
 )
+
+-- Brightness - Requires brightnessctl
 hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
 hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
 
